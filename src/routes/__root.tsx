9a8a65dd847f97341dc5bf47556d8407101e7f2c
Aakasha063 <aakasha063@gmail.com>
2026-08-18 12:11:24 +0530

    Update favicon icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -105,7 +105,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+      { rel: "icon", type: "image/png", href: "/favicon.png" },
       { rel: "apple-touch-icon", href: "/favicon.png" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
