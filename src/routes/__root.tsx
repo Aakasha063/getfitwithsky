2528d14f4dec528023b4250876173ddf542bc648
Aakasha063 <aakasha063@gmail.com>
2026-08-18 12:26:39 +0530

    Fix favicon to use proper SVG without borders

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -105,8 +105,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      { rel: "icon", type: "image/png", href: "/favicon.png" },
-      { rel: "apple-touch-icon", href: "/favicon.png" },
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
